5d2fd0efc0e2a51e21f63449e69adf7c0c7dc498
RoundTable <masterknightzaza@gmail.com>
2025-03-25 20:35:58 +0700

    fixed

--- models/hotel.js
+++ models/hotel.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose');
 const hotelSchema = new mongoose.Schema({
   name: String,
   address: String,
+  district:  String,
+  province: String,
+  postalcode: String,
+  dailyrate: Number,
   phone: String,
+  picture: String,
   averageRating: { type: Number, default: 0 },
   totalReviews: { type: Number, default: 0 }
 });
