9cba50bed53fc55cfcf8f8f8106408b0ab103c6d
RoundTable <masterknightzaza@gmail.com>
2025-03-25 21:34:32 +0700

    fixed2

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/hotels', hotelRoutes);
 app.use('/reviews', reviewRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
